abeeec84d07acad9019a4da7ce241a9bed825016
Ma Tianxiao <matx2215@outlook.com>
2015-11-23 23:59:12 +0800

    add application logic

---
+++ src/core.js
@@ -0,0 +1,44 @@
+import {fromJS} from 'immutable'
+
+export function setEntries(state, entries) {
+  return state.set('entries', fromJS(entries))
+}
+
+function getWinner(vote) {
+  if (!vote) {
+    return []
+  }
+  const [a, b] = vote.get('pair')
+  const aVotes = vote.getIn(['tally', a], 0)
+  const bVotes = vote.getIn(['tally', b], 0)
+
+  if (aVotes > bVotes) {
+    return [a]
+  } else if (aVotes < bVotes) {
+    return [b]
+  } else {
+    return [a, b]
+  }
+}
+
+export function next(state) {
+  const entries = state.get('entries')
+                 .concat(getWinner(state.get('vote')))
+
+  if (entries.size == 1) {
+    return state.remove('entries')
+                .remove('vote')
+                .set('winner', entries.first())
+  }
+
+  return state.merge({
+    vote: {
+      pair: entries.take(2)
+    },
+    entries: entries.skip(2)
+  })
+}
+
+export function vote(state, entry) {
+  return state.updateIn(['vote', 'tally', entry], 0, tally => tally + 1)
+}
---
+++ test/core_spec.js
@@ -0,0 +1,145 @@
+import {expect} from 'chai'
+import {fromJS} from 'immutable'
+
+import {setEntries, next, vote} from '../src/core'
+
+describe('application logic', () => {
+
+  describe('setEntries', () => {
+
+    it('adds the entries to the state', () => {
+      const state = fromJS({})
+      const entries = ['a', 'b']
+      const nextState = setEntries(state, entries)
+
+      expect(nextState).to.equal(fromJS({
+        entries: ['a', 'b']
+      }))
+    })
+  })
+
+  describe('next', () => {
+
+    it('takes the next two entries under vote', () => {
+      const state = fromJS({
+        entries: ['a', 'b', 'c']
+      })
+      const nextState = next(state)
+
+      expect(nextState).to.equal(fromJS({
+        entries: ['c'],
+        vote: {
+          pair: ['a', 'b']
+        }
+      }))
+    })
+
+    it('puts winner of current vote back to entries', () => {
+      const state = fromJS({
+        entries: ['c'],
+        vote: {
+          pair: ['a', 'b'],
+          tally: {
+            a: 2,
+            b: 3
+          }
+        }
+      })
+      const nextState = next(state)
+
+      expect(nextState).to.equal(fromJS({
+        entries: [],
+        vote: {
+          pair: ['c', 'b']
+        }
+      }))
+    })
+
+    it('puts both from tied vote back to entries', () => {
+      const state = fromJS({
+        entries: ['c'],
+        vote: {
+          pair: ['a', 'b'],
+          tally: {
+            a: 2,
+            b: 2
+          }
+        }
+      })
+      const nextState = next(state)
+
+      expect(nextState).to.equal(fromJS({
+        entries: ['b'],
+        vote: {
+          pair: ['c', 'a']
+        }
+      }))
+    })
+
+    it('marks winner when just one entry left', () => {
+      const state = fromJS({
+        entries: [],
+        vote: {
+          pair: ['a', 'b'],
+          tally: {
+            a: 3,
+            b: 2
+          }
+        }
+      })
+      const nextState = next(state)
+
+      expect(nextState).to.equal(fromJS({
+        winner: 'a'
+      }))
+    })
+  })
+
+  describe('vote', () => {
+
+    it('creates a tally for voted entry', () => {
+      const state = fromJS({
+        entries: [],
+        vote: {
+          pair: ['a', 'b'],
+        }
+      })
+      const nextState = vote(state, 'a')
+
+      expect(nextState).to.equal(fromJS({
+        entries: [],
+        vote: {
+          pair: ['a', 'b'],
+          tally: {
+            a: 1
+          }
+        }
+      }))
+    })
+
+    it('adds to existing tally for the voted entry', () => {
+      const state = fromJS({
+        entries: [],
+        vote: {
+          pair: ['a', 'b'],
+          tally: {
+            a: 1,
+            b: 3
+          }
+        }
+      })
+      const nextState = vote(state, 'a')
+
+      expect(nextState).to.equal(fromJS({
+        entries: [],
+        vote: {
+          pair: ['a', 'b'],
+          tally: {
+            a: 2,
+            b: 3
+          }
+        }
+      }))
+    })
+  })
+})
